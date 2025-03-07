a66cf759c6e9696998af3425cd605eafed446e78
Mukesh Swain <mukeshswain2844@gmail.com>
2025-03-07 11:11:22 +0530

    commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+
+const app = express();  
+
+app.get("/", (req, res) => {
+    res.send("Hello from the backend");
+});
+
+app.listen(3000, () => {
+    console.log(`server is runing on localhost http://localhost:3000`);
+});
